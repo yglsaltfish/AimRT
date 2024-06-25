c8b5bf95fb9afd54926f71bb7e814c523485d45c
wtudio <wtudio@outlook.com>
2024-06-25 12:00:56 +0800

    fix add werror

--- cmake/AddWerror.cmake
+++ cmake/AddWerror.cmake
@@ -1,7 +1,17 @@
-function(add_werror target)
-  if(CMAKE_CXX_COMPILER_ID MATCHES "GNU|Clang")
-    target_compile_options(${target} PRIVATE -Wall -Wextra -Wno-unused-parameter -Wno-missing-field-initializers -Wno-implicit-fallthrough -Werror)
-  elseif(CMAKE_CXX_COMPILER_ID MATCHES "MSVC")
-    # target_compile_options(${target} PRIVATE /W4 /WX) #TODO
+macro(add_werror target)
+  if(AIMRT_BUILD_WITH_WERROR)
+    if(CMAKE_CXX_COMPILER_ID MATCHES "GNU|Clang")
+      target_compile_options(
+        ${target}
+        PRIVATE -Wall
+                -Wextra
+                -Wno-unused-parameter
+                -Wno-missing-field-initializers
+                -Wno-implicit-fallthrough
+                -Wno-stringop-overflow
+                -Werror)
+    elseif(CMAKE_CXX_COMPILER_ID MATCHES "MSVC")
+      target_compile_options(${target} PRIVATE /W4 /WX)
+    endif()
   endif()
-endfunction()
+endmacro()
