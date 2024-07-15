e9e0970f57b1fad265a6c663560ef5c6c5320823
zhangyi <zhangyi@agibot.com>
2024-07-15 13:56:34 +0800

    [aimrt_py] update package procedure

--- src/tools/package_aimrt_py/setup.py
+++ src/tools/package_aimrt_py/setup.py
@@ -1,7 +1,13 @@
+import setuptools
 from setuptools import find_packages, setup
+
+with open("VERSION", "r") as f:
+    version = f.read().strip()
 
 setup(
     packages=find_packages(where="."),
     package_dir={"": "."},
     package_data={"aimrt_py": ["libaimrt_*_plugin.so", "aimrt_py.*.so"]},
+    version=version,
+    ext_modules=[setuptools.Extension(name="dummy", sources=[])],
 )
