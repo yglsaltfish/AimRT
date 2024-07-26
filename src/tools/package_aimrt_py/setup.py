c388b959051eb4ba4b0f39830c69a2a92d6b22f2
zhangyi <zhangyi@agibot.com>
2024-07-26 14:21:38 +0800

    [aimrt_py] 消除find_package警告

--- src/tools/package_aimrt_py/setup.py
+++ src/tools/package_aimrt_py/setup.py
@@ -1,13 +1,10 @@
 import setuptools
-from setuptools import find_packages, setup
+from setuptools import setup
 
 with open("VERSION", "r") as f:
     version = f.read().strip()
 
 setup(
-    packages=find_packages(where="."),
-    package_dir={"aimrt_py": "aimrt_py"},
-    include_package_data=True,
     version=version,
     ext_modules=[setuptools.Extension(name="dummy", sources=[])],
 )
