c08ced27bbe27af612183df5b7b37a4aadbb1940
zhangyi <zhangyi@agibot.com>
2024-07-30 17:16:42 +0800

    remove _deps dir while formatting
    仅格式化根目录（不递归）以及src目录下的python文件和cmake文件

--- format.sh
+++ format.sh
@@ -14,9 +14,10 @@ find ./src -regex '.*\.cc\|.*\.h\|.*\.proto' -and -not -regex '.*\.pb\.cc\|.*\.p
 echo "clang-format done"
 
 # cmake-format, apt install cmake-format
-find ./ -regex '.*\.cmake\|.*CMakeLists\.txt$' -and -not -regex '\./build/.*\|\./document/.*' | xargs cmake-format -c ./.cmake-format.py -i
+{ find . -maxdepth 1 -name "CMakeLists.txt"; find ./src -name "CMakeLists.txt"; } | xargs cmake-format -c ./.cmake-format.py -i
+{ find ./cmake -name "*.cmake"; find ./src -name "*.cmake"; } | xargs cmake-format -c ./.cmake-format.py -i
 echo "cmake-format done"
 
 # autopep8, apt install python3-autopep8
-find ./ -regex '.*\.py' -and -not -regex '\./build/.*\|\./document/.*' | xargs autopep8 -i --global-config ./.pycodestyle
+{ find . -maxdepth 1 -name "*.py" -print; find ./src -name "*.py" -print; } | xargs autopep8 -i --global-config ./.pycodestyle
 echo "python format done"
