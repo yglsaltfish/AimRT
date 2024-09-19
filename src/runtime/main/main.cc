10fc5bb8201de9bed8f5fb1b54b8bc139231ac60
zhangyi <zhangyi@agibot.com>
2024-09-19 11:15:05 +0800

    feat: Add versioning and improve command line help

--- src/runtime/main/main.cc
+++ src/runtime/main/main.cc
@@ -20,6 +20,12 @@ DEFINE_string(dump_init_report_path, "./init_report.txt", "dump init report path
 DEFINE_bool(register_signal, true, "register handle for sigint and sigterm");
 DEFINE_int32(running_duration, 0, "running duration, seconds");
 
+DEFINE_bool(h, false, "help");
+DEFINE_bool(v, false, "version");
+DECLARE_bool(help);
+DECLARE_bool(helpshort);
+DECLARE_bool(version);
+
 using namespace aimrt::runtime::core;
 
 AimRTCore* global_core_ptr_ = nullptr;
@@ -33,8 +39,46 @@ void SignalHandler(int sig) {
   raise(sig);
 };
 
+void PrintVersion() {
+  std::cout << "AimRT Version: " << AIMRT_VERSION << std::endl;
+}
+
+void PrintUsage() {
+  std::cout << "OVERVIEW: AimRT is a high-performance runtime framework for modern robotics.\n\n"
+               "VERSION: " AIMRT_VERSION
+               "\n\nUSAGE: aimrt_main --cfg_file_path=<string> [options]\n\n"
+               "OPTIONS:\n\n"
+               "Generic Options:\n\n"
+               "  --version                         - Show version\n"
+               "  --help                            - Show help\n\n"
+               "AimRT Options:\n\n"
+               "  --cfg_file_path=<string>          - Path to the configuration file, default is empty\n"
+               "  --dump_cfg_file                   - Dump the configuration file to a file, default is false\n"
+               "  --dump_cfg_file_path=<string>     - Path to dump the configuration file, default is ./dump_cfg.yaml\n"
+               "  --dump_init_report                - Dump the initialization report, default is false\n"
+               "  --dump_init_report_path=<string>  - Path to dump the initialization report, default is ./init_report.txt\n"
+               "  --register_signal                 - Register signal handlers for SIGINT and SIGTERM, default is true\n"
+               "  --running_duration=<int>          - Running duration in seconds, default is 0 which means running forever\n";
+}
+
+void HandleCommandLineFlags(int32_t argc, char** argv) {
+  if (argc == 1) {
+    PrintUsage();
+    exit(0);
+  }
+  gflags::ParseCommandLineNonHelpFlags(&argc, &argv, true);
+  if (FLAGS_version || FLAGS_v) {
+    PrintVersion();
+    exit(0);
+  }
+  if (FLAGS_help || FLAGS_h) {
+    PrintUsage();
+    exit(0);
+  }
+}
+
 int32_t main(int32_t argc, char** argv) {
-  gflags::ParseCommandLineFlags(&argc, &argv, true);
+  HandleCommandLineFlags(argc, argv);
 
   if (FLAGS_register_signal) {
     signal(SIGINT, SignalHandler);
