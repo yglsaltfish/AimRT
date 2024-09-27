45061987820d399da6a6d38fe07cbb3fb17280ed
zhangyi <zhangyi@agibot.com>
2024-09-27 13:20:34 +0800

    feat: enhance versioning support for AimRT

--- src/plugins/grpc_plugin/grpc_rpc_backend.cc
+++ src/plugins/grpc_plugin/grpc_rpc_backend.cc
@@ -21,6 +21,7 @@
 #include "aimrt_module_cpp_interface/rpc/rpc_status.h"
 #include "core/rpc/rpc_backend_tools.h"
 #include "core/rpc/rpc_invoke_wrapper.h"
+#include "core/util/version.h"
 #include "grpc_plugin/client/options.h"
 #include "grpc_plugin/global.h"  // IWYU pragma: keep
 #include "grpc_plugin/grpc/message.h"
@@ -86,6 +87,8 @@ namespace aimrt::plugins::grpc_plugin {
 namespace asio = boost::asio;
 namespace util = aimrt::common::util;
 namespace chrono = std::chrono;
+
+using aimrt::runtime::core::util::GetAimRTVersion;
 
 namespace {
 
@@ -387,7 +390,7 @@ void GrpcRpcBackend::Invoke(
             req_ptr->SetMethod("POST");
             req_ptr->AddHeader("content-type", "application/grpc");
             req_ptr->AddHeader("te", "trailers");
-            req_ptr->AddHeader("user-agent", std::string("grpc-c++-aimrt/") + AIMRT_VERSION);
+            req_ptr->AddHeader("user-agent", std::string("grpc-c++-aimrt/") + GetAimRTVersion());
 
             auto timeout = client_invoke_wrapper_ptr->ctx_ref.Timeout();
             if (timeout <= chrono::nanoseconds(0)) {
